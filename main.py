b5b78cc6e1bf97ce49f193030bcf2c8c83abdd71
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2023-08-11 00:09:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import sys
 
-def hello(a,b):
-    print("Hello and that's your sum: {}".format(a + b))
-
 def main():
-    arguments = len(sys.argv) - 1
-    if arguments==2:
-        print("Number of arguments: {} arguments".format(arguments))
-        a = int(sys.argv[1])
-        b = int(sys.argv[2])
-        hello(a, b)
+    args = len(sys.argv) - 1
+
+    if args > 0:
+        print("The script was called with %i arguments" % (args))
+
+        for pos in range(1, args + 1):
+            arg = sys.argv[pos]
+            print("Argument %i is %s" % (pos, arg))
+
     else:
-        raise ValueError('Two arguments required')
+        print("No command line arguments provided")
 
 if __name__ == "__main__":
     main()
