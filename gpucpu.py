1bed85f7c62cd1574a9cedf8689f2d2f6c7200b2
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2024-11-06 16:30:08 +0100

    Add gpucpu.py

---
+++ gpucpu.py
@@ -0,0 +1,28 @@
+from numba import jit
+import numpy as np
+from timeit import default_timer as timer
+
+def cpufunction(matrix):
+    n = matrix.shape[0]
+    for i in range(n):
+        matrix[i] += np.tanh(np.sin(i * np.pi) * np.cos(i * np.pi))
+
+@jit(nopython=True)
+def gpufunction(matrix):
+    n = matrix.shape[0]
+    for i in range(n):
+        matrix[i] += np.tanh(np.sin(i * np.pi) * np.cos(i * np.pi))
+
+if __name__ == '__main__':
+    N = 10_000_000
+    a = np.zeros(N, dtype=np.float64)
+
+    print(f'Start programu.')
+
+    start = timer()
+    cpufunction(a)
+    print(f'Czas wykonania na CPU: {(timer() - start):.2f} s')
+
+    start = timer()
+    gpufunction(a)
+    print(f'Czas wykonania na GPU: {(timer() - start):.2f} s')
