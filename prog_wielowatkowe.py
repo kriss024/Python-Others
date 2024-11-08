4b0ff98e9dde3382db54b44797dd1ed90a6b7ad8
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2024-11-08 10:53:51 +0100

    Update prog_wielowatkowe.py

--- prog_wielowatkowe.py
+++ prog_wielowatkowe.py
@@ -4,10 +4,10 @@ import random
 
 random.seed(12345)
 
-# Shared variable
-shared_data = 0
+# Shared variable - must be a list, because scalars are not mutable!
+shared_data = [0]
 
-n_range = 30
+max_range = 30
 
 #~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.
 
@@ -15,30 +15,33 @@ n_range = 30
 data_lock = threading.Lock()
 
 # Writer thread function
-def writer(name: str):
-    global shared_data
-    for i in range(n_range):
+def writer(max_range: int):
+    while shared_data[0] <= max_range:
         with data_lock:  # Lock access to shared_data
-            shared_data += 1
-            print(f"{name} updated shared_data to {shared_data}")
+            shared_data[0] += 1
+            print(f"Writer updated shared_data to {shared_data[0]}")
 
         time.sleep(random.random())
+    else:
+        print('Loop done.')
 
 # Reader thread function
-def reader(name: str):
-    for i in range(n_range):
+def reader(max_range: int):
+    while shared_data[0] <= max_range:
         with data_lock:  # Lock access to shared_data
-            print(f"\t{name} read shared_data as {shared_data}")
+            print(f"\tReader read shared_data as {shared_data[0]}")
 
         time.sleep(random.random())
+    else:
+        print('Loop done.')
 
 #~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.
 
 print("Starting the program.")
 
 # Create threads
-writer_thread = threading.Thread(target=writer, args=("Writer",))
-reader_thread = threading.Thread(target=reader, args=("Reader",))
+writer_thread = threading.Thread(target=writer, args=(max_range,))
+reader_thread = threading.Thread(target=reader, args=(max_range,))
 
 # Start threads
 writer_thread.start()
