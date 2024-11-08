0b9471666d2689ea7079bffc970811dc6537beb4
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2024-11-08 15:25:30 +0100

    Update julia_python.py

--- julia_python.py
+++ julia_python.py
@@ -4,5 +4,15 @@ julia.install()
 from julia.api import Julia
 jl = Julia(compiled_modules=False)
 
-sin_90 = jl.eval("sin(90)")
-print(f'sin(90) = {sin_90:.5f}')
+mass = 80
+
+julia_code = f"""
+mass = {mass}
+G = 6.67
+c = 2.99  
+R =  (2 * G * mass) / (c^2)
+"""
+
+result = jl.eval(julia_code)
+print(type(result))
+print(f'result = {result:.6f}')
