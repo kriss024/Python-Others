25c72de7a0d48117f6b12ddaa546762493d20155
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2024-11-17 10:32:55 +0100

    Update Connect_to_db.py

--- Connect_to_db.py
+++ Connect_to_db.py
@@ -1,32 +1,41 @@
-import pprint
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
 
-__author__ = 'Superuser'
+try:
+    connection = mysql.connector.connect(
+                                        host="127.0.0.1",
+                                        port=3306,
+                                        user="user",
+                                        password="userpassword",
+                                        database="database"
+                                    )
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("-- Connected to MySQL Server version ", db_Info)
 
+        cursor = connection.cursor()
 
-import psycopg2
-import sys
+        cursor.execute("select database()")
+        record = cursor.fetchone()
+        print(f"You're connected to database: {record[0]}")
 
-def main():
+        cursor.execute("select curdate()")
+        record = cursor.fetchone()
+        print(f"Current date is: {record[0]}")
 
-    conn_string = "host='localhost' dbname='postgres' user='postgres' password='root'"
+        cursor.close()
 
-    print "Connecting to database\n	->%s" % (conn_string)
+except Error as error:
+    if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("-- Something is wrong with your user name or password")
+    elif error.errno == errorcode.ER_BAD_DB_ERROR:
+        print("-- Database does not exist")
+    else:
+        print(error)
 
-    conn = psycopg2.connect(conn_string)
-    cursor = conn.cursor()
-    print "Connected!\n"
-
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM weather")
-    records = cursor.fetchall()
-
-    print "\nShow me the databases:\n"
-
-
-    print "\nRows: \n"
-    for row in records:
-
-        print row[0],row[1],row[2],row[3],row[4]
-
-if __name__ == "__main__":
-    main()
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("-- MySQL connection is closed")
